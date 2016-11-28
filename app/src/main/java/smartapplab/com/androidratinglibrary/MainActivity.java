54f32797249331061596fd4932786c11c14ce541
Amit Kumar <tomar.iitr@gmail.com>
2016-11-28 16:52:28 +0530

    Initial commit

---
+++ app/src/main/java/smartapplab/com/androidratinglibrary/MainActivity.java
@@ -0,0 +1,13 @@
+package smartapplab.com.androidratinglibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
